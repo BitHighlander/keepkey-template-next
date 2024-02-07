33328843007d8f00b0222abfe0355979b7585230
bithighlander <bithighlander@gmail.com>
2024-02-06 20:06:00 -0500

    begin ux

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        VITE_COVALENT_API_KEY: process.env.VITE_COVALENT_API_KEY,
+        VITE_ETHPLORER_API_KEY: process.env.VITE_ETHPLORER_API_KEY,
+        VITE_BLOCKCHAIR_API_KEY: process.env.VITE_BLOCKCHAIR_API_KEY,
+    },
+    // Include any other Next.js config options you need
+};
 
 export default nextConfig;
